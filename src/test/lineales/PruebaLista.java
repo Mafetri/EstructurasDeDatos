7935a13988854bd8b43ef2f95e21ca410b1e3419
Manuel Triñanes <manutritri2@gmail.com>
2021-04-13 15:32:12 -0300

    Funcional a medias

--- src/test/lineales/PruebaLista.java
+++ src/test/lineales/PruebaLista.java
@@ -1,6 +1,4 @@
 package test.lineales;
-
-import jdk.nashorn.api.tree.WhileLoopTree;
 import lineales.dinamicas.*;
 
 public class PruebaLista {
@@ -18,24 +16,25 @@ public class PruebaLista {
         l2.insertar(6, 3);
         l2.insertar(7, 4);
 
-        l3.insertar(9,1);
-        l3.insertar(6,2);
-        l3.insertar(5,3);
-        l3.insertar(0,4);
-        l3.insertar(9,5);
-        l3.insertar(6,6);
-        l3.insertar(5,7);
-        l3.insertar(0,8);
-        l3.insertar(5,9);
-        l3.insertar(6,10);
-        l3.insertar(9,11);
+        l3.insertar(9, 1);
+        l3.insertar(6, 2);
+        l3.insertar(5, 3);
+        l3.insertar(0, 4);
+        l3.insertar(9, 5);
+        l3.insertar(6, 6);
+        l3.insertar(5, 7);
+        l3.insertar(0, 8);
+        l3.insertar(5, 9);
+        l3.insertar(6, 10);
+        l3.insertar(9, 11);
 
         Lista concatenada = concatenar(l1, l2).clone();
+
 
         System.out.println("Cadena 1: " + l1.toString());
         System.out.println("Cadena 2: " + l2.toString());
         System.out.println("Cadena concatenada: " + concatenada.toString());
-        System.out.println("Lista: "+ l3.toString());
+        System.out.println("Lista: " + l3.toString());
 
         System.out.println(comprobar(l3));
     }
@@ -57,7 +56,6 @@ public class PruebaLista {
 
     public static boolean comprobar(Lista l1) {
         int i = 1;
-        int longitudCadena;
         int coincidentes = 0;
         int coincidentesInvertidos = 0;
         Cola cAux = new Cola();
@@ -67,34 +65,35 @@ public class PruebaLista {
         if (l1.localizar(0) > 0) {
             // Mientras que no llegue al maximo de l1 y la posicion de la lista en i
             // sea distinta de 0
-            while ((int)l1.recuperar(i) != 0) {
-                cAux.poner(l1.recuperar(i));
-                pAux.apilar(l1.recuperar(i));
-                i++;
+            while ((int) l1.recuperar(1) != 0) {
+                cAux.poner(l1.recuperar(1));
+                pAux.apilar(l1.recuperar(1));
+                l1.eliminar(1);
             }
 
-            // i esta posicionado en el int 0,
-            longitudCadena = i;
-            i++;
+            l1.eliminar(1);
 
             // Mientras el frente de la cola sea igual al proximo elemento de la lista
-            while (cAux.obtenerFrente() == l1.recuperar(i) && cAux != null) {
+            while (cAux.obtenerFrente() == l1.recuperar(1) && cAux != null) {
                 coincidentes++;
+                l1.eliminar(1);
                 cAux.sacar();
             }
 
-            i++;
+            // Si la posicion 1 en la que quedo la lista es 0, quiere decir que la segunda
+            // cadena coincidio en numero de posiciones con la primera
+            if ((int) l1.recuperar(1) == 0) {
+                // Elimino el supuesto 0 que habra quedado en la primera posicion
+                l1.eliminar(1);
 
-            if (coincidentes == longitudCadena) {
+                //Mientras que el tope de la pila sea el elemento siguiente de la lista
                 while (pAux.obtenerTope() == l1.recuperar(i)) {
                     coincidentesInvertidos++;
+                    l1.eliminar(1);
                     pAux.desapilar();
-                    i++;
                 }
             }
-
         }
         return coincidentes == coincidentesInvertidos;
-
     }
 }
