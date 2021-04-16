3bba04f209096a4ce2b6ac0aa86cafc407879146
Manuel Triñanes <manutritri2@gmail.com>
2021-04-16 17:11:48 -0300

    Arreglos y comentarios

--- src/lineales/dinamicas/Cola.java
+++ src/lineales/dinamicas/Cola.java
@@ -15,154 +15,159 @@ package lineales.dinamicas;
 public class Cola {
     private Nodo frente;
     private Nodo fin;
-    
-    //---- Constructor ----
-    public Cola(){
-        //Creo la cola con el frente y el fin ambos en nulo.
+
+    // ---- Constructor ----
+    public Cola() {
+        // Creo la cola con el frente y el fin ambos en nulo.
         this.frente = null;
         this.fin = null;
     }
-    
-    //---- Poner ----
-    public boolean poner(Object elemento){
+
+    // ---- Poner ----
+    // Coloca el elemento enviado en la siguiente posicion de la cola
+    public boolean poner(Object elemento) {
         Nodo nuevo = new Nodo(elemento, null);
-        
-        //Si la cola esta vacia
-        if(this.fin == null){
-            //Apunto el fin y el frente de la cola a este nuevo elemento
+
+        // Si la cola esta vacia
+        if (this.fin == null) {
+            // Apunto el fin y el frente de la cola a este nuevo elemento
             this.fin = nuevo;
             this.frente = nuevo;
-        }else{ 
-            //Seteo el enlace del ultimo elemento (this.fin.setEnlace) a al nuevo nodo
+        } else {
+            // Seteo el enlace del ultimo elemento (this.fin.setEnlace) a al nuevo nodo
             this.fin.setEnlace(nuevo);
-            //Y ahora apunto fin a nuevo
+            // Y ahora apunto fin a nuevo
             this.fin = nuevo;
         }
-        
+
         return true;
     }
-    
-    //---- Sacar ----
-    public boolean sacar(){
+
+    // ---- Sacar ----
+    // Saca el elemento en el frente de la cola
+    public boolean sacar() {
         boolean exito = true;
-        
-        //Si la cola esta vacia, "exito" es false
-        if(this.frente == null){
+
+        // Si la cola esta vacia, "exito" es false
+        if (this.frente == null) {
             exito = false;
-        }else{
-            //Caso contrario, apunto frente a su mismo enlace (proximo nodo)
+        } else {
+            // Caso contrario, apunto frente a su mismo enlace (proximo nodo)
             this.frente = this.frente.getEnlace();
-            //En caso de que el nuevo frente sea null, fin tambien tiene que se null
-            if(this.frente == null){
+            // En caso de que el nuevo frente sea null, fin tambien tiene que se null
+            if (this.frente == null) {
                 this.fin = null;
             }
         }
-        
+
         return exito;
     }
-    
-    //---- Obtener Frente ----
-    public Object obtenerFrente(){
-        Object elemento;
-        
-        if(this.frente != null){
+
+    // ---- Obtener Frente ----
+    // Devuelve el frente de la cola
+    public Object obtenerFrente() {
+        Object elemento = null;
+
+        if (this.frente != null) {
             elemento = this.frente.getElem();
-        }else{
-            elemento = "COLA VACIA";
         }
-        
+
         return elemento;
     }
-    
-    //---- Es Vacia? ----
-    public boolean esVacia(){
+
+    // ---- Es Vacia? ----
+    // Retorna true en caso de que sea vacia o false en caso contrario
+    public boolean esVacia() {
         return this.frente == null;
     }
-    
-    //---- Vaciar -----
-    public void vaciar(){
+
+    // ---- Vaciar -----
+    // Vacia la cola
+    public void vaciar() {
         this.frente = null;
         this.fin = null;
     }
-    
-    //---- Clone ----
-    public Cola clone(){
+
+    // ---- Clone ----
+    // Devuelve una cola clonada de la original
+    public Cola clone() {
         Cola clonada = new Cola();
         Nodo aux1, aux2, nuevo;
-        
-        //Si no hay nodo frente, entonces esta vacia y clonada quedara vacia
-        if(this.frente != null){
-            //Apunto aux1 al frente de la cola original
+
+        // Si no hay nodo frente, entonces esta vacia y clonada quedara vacia
+        if (this.frente != null) {
+            // Apunto aux1 al frente de la cola original
             aux1 = this.frente;
-            
-            //Asigno el frente y fin de clonada al elemento de aux1 y con enlace nulo
+
+            // Asigno el frente y fin de clonada al elemento de aux1 y con enlace nulo
             clonada.frente = new Nodo(aux1.getElem(), null);
             clonada.fin = clonada.frente;
-            
-            //Apunto aux2 al fin de clonada
+
+            // Apunto aux2 al fin de clonada
             aux2 = clonada.fin;
-            
-            //Apunto aux1 al siguiente nodo
+
+            // Apunto aux1 al siguiente nodo
             aux1 = aux1.getEnlace();
-            
-            //Si aux1 es nulo, entonces no hay otro nodo para clonar
-            while(aux1 != null){
-                //Inicializo al nodo nuevo con el elemento de aux1 y con el enlace nulo
+
+            // Si aux1 es nulo, entonces no hay otro nodo para clonar
+            while (aux1 != null) {
+                // Inicializo al nodo nuevo con el elemento de aux1 y con el enlace nulo
                 nuevo = new Nodo(aux1.getElem(), null);
 
-                //Aux2 lo enlazo al nuevo nodo
+                // Aux2 lo enlazo al nuevo nodo
                 aux2.setEnlace(nuevo);
-                
-                //Apunto al final de la cola clonada al ultimo nodo creado (nuevo)
+
+                // Apunto al final de la cola clonada al ultimo nodo creado (nuevo)
                 clonada.fin = nuevo;
 
-                //Ahora aux2 lo apunto al proximo nodo de la cola
+                // Ahora aux2 lo apunto al proximo nodo de la cola
                 aux2 = aux2.getEnlace();
-                
-                //Ahora aux1 lo apunto al proximo nodo
+
+                // Ahora aux1 lo apunto al proximo nodo
                 aux1 = aux1.getEnlace();
             }
         }
-        
+
         return clonada;
     }
-    
-    //---- toString ----
-    public String toString(){
+
+    // ---- toString ----
+    // Devulve en string la cola entera
+    public String toString() {
         String enTexto = "[ ";
-        
-        if(this.frente == null){
+
+        if (this.frente == null) {
             enTexto += " COLA VACIA ";
-        }else{
-            //Creo un nodo auxiliar apuntando al frente de la cola
+        } else {
+            // Creo un nodo auxiliar apuntando al frente de la cola
             Nodo aux = this.frente;
-            
-            //Si hay un solo elemento en la cola entonces
-            if(this.fin == this.frente){
-                //Agrego ese elemento a la cadena
+
+            // Si hay un solo elemento en la cola entonces
+            if (this.fin == this.frente) {
+                // Agrego ese elemento a la cadena
                 enTexto += aux.getElem().toString();
-                //Y hago que aux apunte a su enlace nulo
+                // Y hago que aux apunte a su enlace nulo
                 aux = aux.getEnlace();
             }
-            
-            //Mientras que aux no sea nulo
-            while(aux != null){
-                //Coloco el elemento de aux en la cadena enTexto
+
+            // Mientras que aux no sea nulo
+            while (aux != null) {
+                // Coloco el elemento de aux en la cadena enTexto
                 enTexto += aux.getElem().toString();
-                //Apunto aux a su propio enlace, si este es null entonces aux
-                //se comvertira en null haciendo que el while se corte
+                // Apunto aux a su propio enlace, si este es null entonces aux
+                // se comvertira en null haciendo que el while se corte
                 aux = aux.getEnlace();
-                
-                //Si aux no es null, entonces se espera un proximo elemento 
-                //colocando una coma y espacio a la cadena
-                if(aux != null){
+
+                // Si aux no es null, entonces se espera un proximo elemento
+                // colocando una coma y espacio a la cadena
+                if (aux != null) {
                     enTexto += ", ";
                 }
             }
         }
-        
+
         enTexto += " ]";
-        
+
         return enTexto;
     }
 }
