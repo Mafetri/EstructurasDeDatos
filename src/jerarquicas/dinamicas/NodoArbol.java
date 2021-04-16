b137d6ead177171ba3269332efe41e47dbb55f18
Manuel Triñanes <manutritri2@gmail.com>
2021-04-16 20:19:48 -0300

    Implementacion Completa

---
+++ src/jerarquicas/dinamicas/NodoArbol.java
@@ -0,0 +1,37 @@
+package jerarquicas.dinamicas;
+
+public class NodoArbol {
+    private Object elem;
+    private NodoArbol izquierdo;
+    private NodoArbol derecho;
+
+    public NodoArbol(Object unElem, NodoArbol izq, NodoArbol der){
+        this.elem = unElem;
+        this.izquierdo = izq;
+        this.derecho = der;
+    }
+
+    public Object getElem(){
+        return this.elem;
+    }
+
+    public NodoArbol getIzquierdo(){
+        return this.izquierdo;
+    }
+
+    public NodoArbol getDerecho(){
+        return this.derecho;
+    }
+
+    public void setElem(Object unElem){
+        this.elem = unElem;
+    }
+
+    public void setIzquierdo(NodoArbol izq){
+        this.izquierdo = izq;
+    }
+
+    public void setDerecho(NodoArbol der){
+        this.derecho = der;
+    }
+}
