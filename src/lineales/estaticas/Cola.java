fc83b241bd98b10f4edc3211593c27ae93571792
Manuel Triñanes <manutritri2@gmail.com>
2021-04-16 17:07:10 -0300

    Arreglos

--- src/lineales/estaticas/Cola.java
+++ src/lineales/estaticas/Cola.java
@@ -6,115 +6,117 @@ public class Cola {
     private int frente;
     private int fin;
     private static final int TAMANIO = 10;
-    
-    public Cola(){
+
+    // ---- Constructor ----
+    public Cola() {
         this.arreglo = new Object[TAMANIO];
         this.frente = 0;
         this.fin = 0;
     }
-    
-    //---- Poner ----
-    public boolean poner(Object elemento){
+
+    // ---- Poner ----
+    // Coloca el elemento enviado en la siguiente posicion de la cola
+    public boolean poner(Object elemento) {
         boolean exito = false;
-        
-        //Si el arreglo no esta lleno, osea que la posicion siguiente a fin sea
-        //distinto a el frente
-        //(coloco el modulo de tamanio ya que la ultima posicion del arreglo no
-        // debe ser llenada a menos que el frente que esta en 0 sea movido)
-        if(((this.fin + 1) % TAMANIO) != this.frente){
-            //Al final de la cola le agrego el nuevo elemento
+
+        // Si el arreglo no esta lleno, osea que la posicion siguiente a fin sea
+        // distinto a el frente (el mod tamanio es para lograr la circularidad del arreglo)
+        if (((this.fin + 1) % TAMANIO) != this.frente) {
+            // Al final de la cola le agrego el nuevo elemento
             arreglo[fin] = elemento;
-            //Ahora el puntero de final es la proxima posicion
-            //Y cuando llega al maximo (10) al hacer MOD TAMANIO, da 0, entonces
-            //fin va a valer 0 cuando llegue a posicion 10
-            fin = (fin+1) % TAMANIO;
-            
+            // Ahora el puntero de final es la proxima posicion
+            // Y cuando llega al maximo (10) al hacer MOD TAMANIO, da 0, entonces
+            // fin va a valer 0 cuando llegue a posicion 10
+            fin = (fin + 1) % TAMANIO;
+
             exito = true;
         }
-        
+
         return exito;
     }
-    
-    //---- Sacar ----
-    public boolean sacar(){
+
+    // ---- Sacar ----
+    // Saca el elemento en el frente de la cola
+    public boolean sacar() {
         boolean exito;
-        
-        if(this.esVacia()){
+
+        if (this.esVacia()) {
             exito = false;
-        }else{
-            this.arreglo[frente] = null;
-            //Cuando frente sea 10, al hacer mod tamanio, va a dar 0
-            frente = (frente + 1) % TAMANIO;
+        } else {
+            this.arreglo[this.frente] = null;
+            // Cuando frente sea 10, al hacer mod tamanio, va a dar 0
+            this.frente = (this.frente + 1) % TAMANIO;
             exito = true;
         }
-        
+
         return exito;
     }
-    
-    //---- Obtener Frente ----
-    public Object obtenerFrente(){
-        Object elFrente = "Cola Vacia!";
-        
-        if(this.frente != this.fin){
+
+    // ---- Obtener Frente ----
+    // Devuelve el frente de la cola
+    public Object obtenerFrente() {
+        Object elFrente = null;
+
+        if (this.frente != this.fin) {
             elFrente = arreglo[frente];
         }
-        
+
         return elFrente;
     }
-    
-    //---- Es Vacia ---- 
-    public boolean esVacia(){
+
+    // ---- Es Vacia ----
+    // Retorna true en caso de que sea vacia o false en caso contrario
+    public boolean esVacia() {
         return this.frente == this.fin;
     }
-    
-    //---- Vaciar ----
-    public void vaciar(){
+
+    // ---- Vaciar ----
+    // Vacia la cola
+    public void vaciar() {
         this.arreglo = null;
         this.fin = 0;
         this.frente = 0;
     }
-    
-    //---- Clone ----
-    public Cola clone(){
+
+    // ---- Clone ----
+    // Devuelve una cola clonada de la original
+    public Cola clone() {
         Cola clonada = new Cola();
-        int frenteClonadoMovil = this.frente;
-        int finClonado = this.fin;
-        
-        //Lleno la cola hasta el frente con null
-        for(int i=0; i<frenteClonadoMovil;i++){
-            clonada.poner(null);
+        int fin = this.frente;
+
+        while(this.fin != fin){
+            clonada.arreglo[fin] = this.arreglo[fin];
+            fin = (fin+1) % TAMANIO;
         }
-        
-        //Una vez que llegue a frente
-        while(frenteClonadoMovil != finClonado){
-            clonada.poner(this.arreglo[frenteClonadoMovil]); 
-            frenteClonadoMovil = (frenteClonadoMovil+1) % TAMANIO;
-        }
-        
+
+        clonada.fin = this.fin;
+        clonada.frente = this.frente;
+
         return clonada;
     }
-    
-    //---- toString ----
-    public String toString(){
+
+    // ---- toString ----
+    // Devulve en string la cola entera
+    public String toString() {
         String enTexto = "[ ";
         int i = this.frente;
-        
-        if(this.esVacia() != true){
-            while(i != this.fin){
-                if(i != this.frente){
+
+        if (this.esVacia() != true) {
+            while (i != this.fin) {
+                if (i != this.frente) {
                     enTexto += ", " + arreglo[i].toString();
-                }else{
+                } else {
                     enTexto += arreglo[i].toString();
                 }
-                i = (i+1) % TAMANIO;
+                i = (i + 1) % TAMANIO;
             }
 
             enTexto += " ]";
-        }else{
-            enTexto += " LA COLA ESTA VACIA ]";
+        } else {
+            enTexto += " COLA VACIA ]";
         }
-        
+
         return enTexto;
     }
-    
+
 }
