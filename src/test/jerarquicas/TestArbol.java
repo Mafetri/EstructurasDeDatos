9bedc46b305bf104da3ff999d3a5e148bc8da239
Manuel Triñanes <manutritri2@gmail.com>
2021-04-17 19:03:16 -0300

    Aumento de funciones

--- src/test/jerarquicas/TestArbol.java
+++ src/test/jerarquicas/TestArbol.java
@@ -1,8 +1,9 @@
 package test.jerarquicas;
+
 import jerarquicas.dinamicas.*;
 
 public class TestArbol {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // ARBOL FAM: https://imgur.com/a/byUNAhM
         ArbolBin fam = new ArbolBin();
         ArbolBin fam2 = new ArbolBin();
@@ -16,23 +17,25 @@ public class TestArbol {
         fam.insertar("Curie", "Lucia", 'D');
         fam.insertar("Rocco", "Lucia", 'I');
         fam.insertar("Rodrigo", "Alfredo", 'I');
-
+        
         fam2 = fam.clone();
 
-        System.out.println(fam.toString());
-        System.out.println(fam.altura());
-        System.out.println(fam.nivel("Hector")); 
+        System.out.println("Familia 1: \n" + fam.toString());
+        System.out.println("Familia altura: " + fam.altura());
+        System.out.println("Familia nivel de Rocco: " + fam.nivel("Rocco"));
 
         fam.vaciar();
+        System.out.println("\n");
 
-        System.out.println(fam2.toString());
-        System.out.println(fam2.altura());
-        System.out.println(fam2.nivel("Hector")); 
+        System.out.println("Familia 2: \n" + fam2.toString());
+        System.out.println("Familiar 2 Altura: " + fam2.altura());
+        System.out.println("Familia 2 Nivel de Rocco: " + fam2.nivel("Rocco"));
 
-        System.out.println(fam2.toString());
+        System.out.println("\n");
 
-        System.out.println(fam2.preOrden().toString());
-        System.out.println(fam2.inOrden().toString());
-        System.out.println(fam2.posOrden().toString());
+        System.out.println("PreOrden: \n" + fam2.preOrden().toString());
+        System.out.println("InOrden: \n" + fam2.inOrden().toString());
+        System.out.println("PosOrden: \n" + fam2.posOrden().toString());
+        System.out.println("Por nivel: \n" + fam2.listarNiveles().toString());
     }
 }
