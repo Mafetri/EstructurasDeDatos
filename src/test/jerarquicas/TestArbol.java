2dbd886dc8b1a76f7292fdda626474ad6d57cf3d
Manuel Triñanes <manutritri2@gmail.com>
2021-04-17 13:36:12 -0300

    Nada Interesante

---
+++ src/test/jerarquicas/TestArbol.java
@@ -0,0 +1,34 @@
+package test.jerarquicas;
+import jerarquicas.dinamicas.*;
+
+public class TestArbol {
+    public static void main(String[] args){
+        // ARBOL FAM: https://imgur.com/a/byUNAhM
+        ArbolBin fam = new ArbolBin();
+        ArbolBin fam2 = new ArbolBin();
+
+        fam.insertar("Hector", "p", 'z');
+        fam.insertar("Jorge", "Hector", 'I');
+        fam.insertar("Alfredo", "Hector", 'D');
+        fam.insertar("Manuel", "Jorge", 'I');
+        fam.insertar("Lucia", "Jorge", 'D');
+        fam.insertar("Jack", "Manuel", 'I');
+        fam.insertar("Curie", "Lucia", 'D');
+        fam.insertar("Rocco", "Lucia", 'I');
+
+        fam2 = fam.clone();
+
+        System.out.println(fam.preOrden().toString());
+        System.out.println(fam.altura());
+        System.out.println(fam.nivel("Hector")); 
+
+        fam.vaciar();
+
+        System.out.println(fam2.preOrden().toString());
+        System.out.println(fam2.altura());
+        System.out.println(fam2.nivel("Hector")); 
+
+        System.out.println(fam2.toString());
+
+    }
+}
