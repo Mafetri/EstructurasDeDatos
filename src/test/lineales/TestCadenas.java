cd4a2ce1ded33bf2f6d511149e8993ce564b18f0
Manuel Triñanes <manutritri2@gmail.com>
2021-04-18 19:48:44 -0300

    Nada nuevo

--- src/test/lineales/TestCadenas.java
+++ src/test/lineales/TestCadenas.java
@@ -1,10 +1,13 @@
 package test.lineales;
+
 import lineales.dinamicas.*;
 
 public class TestCadenas {
- 
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Cola c1 = new Cola();
+        Cola c2 = new Cola();
+
         c1.poner('A');
         c1.poner('B');
         c1.poner('#');
@@ -15,16 +18,35 @@ public class TestCadenas {
         c1.poner('F');
         System.out.println(generar(c1).toString());
 
+        c2.poner('{');
+        c2.poner('5');
+        c2.poner('+');
+        c2.poner('[');
+        c2.poner('8');
+        c2.poner('*');
+        c2.poner('9');
+        c2.poner('-');
+        c2.poner('(');
+        c2.poner('4');
+        c2.poner('/');
+        c2.poner('2');
+        c2.poner(')');
+        c2.poner('+');
+        c2.poner('7');
+        c2.poner(']');
+        c2.poner('-');
+        c2.poner('1');
+        c2.poner('}');
     }
 
-    public static Cola generar(Cola c1){
+    public static Cola generar(Cola c1) {
         Cola aRetornar = new Cola();
         Pila pAux = new Pila();
         Lista lAux = new Lista();
 
-        while(!c1.esVacia()){
+        while (!c1.esVacia()) {
             // Mientras la cola no este vacia y el frente sea distinto de #
-            while(!c1.esVacia() && (char)c1.obtenerFrente() != '#'){
+            while (!c1.esVacia() && (char) c1.obtenerFrente() != '#') {
                 // Apilo e inserto en la pila y lista auxiliares la cadena
                 pAux.apilar(c1.obtenerFrente());
                 lAux.insertar(c1.obtenerFrente(), 1);
@@ -34,26 +56,25 @@ public class TestCadenas {
 
             // Almaceno en la cola a retornar el ultimo elemento de la lista
             // asi queda en la cola la cadena original
-            for(int i = lAux.longitud(); i >= 1; i--){
+            for (int i = lAux.longitud(); i >= 1; i--) {
                 aRetornar.poner(lAux.recuperar(i));
             }
-
             // Agrego a la cola a retornar la cadena invertida almacenada en la pila
-            while(!pAux.esVacia()){
+            while (!pAux.esVacia()) {
                 aRetornar.poner(pAux.obtenerTope());
                 pAux.desapilar();
             }
 
             // Agrego a la cola la cadena original
-            for(int i = lAux.longitud(); i >= 1; i--){
+            for (int i = lAux.longitud(); i >= 1; i--) {
                 aRetornar.poner(lAux.recuperar(i));
             }
 
             // Mientras no este vacia, coloco # entre las cadenas
-            if(!c1.esVacia()){
+            if (!c1.esVacia()) {
                 aRetornar.poner('#');
             }
-            
+
             // Saco de c1 el # y vacio la lista auxiliar
             c1.sacar();
             lAux.vaciar();
@@ -61,4 +82,5 @@ public class TestCadenas {
 
         return aRetornar;
     }
+
 }
