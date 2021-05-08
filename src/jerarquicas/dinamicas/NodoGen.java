92c5f7871c69a6f63c072fe9434daeebebaeb59d
Manuel Triñanes <manutritri2@gmail.com>
2021-05-08 18:20:27 -0300

    Implementacion completa

---
+++ src/jerarquicas/dinamicas/NodoGen.java
@@ -0,0 +1,38 @@
+package jerarquicas.dinamicas;
+
+public class NodoGen {
+    private Object elem;
+    private NodoGen hijoIzquierdo;
+    private NodoGen hermanoDerecho;
+
+    public NodoGen(Object elemento, NodoGen izq, NodoGen der){
+        elem = elemento;
+        hijoIzquierdo = izq;
+        hermanoDerecho = der;
+    } 
+
+    public Object getElem(){
+        return elem;
+    }
+
+    public NodoGen getHijoIzquierdo(){
+        return hijoIzquierdo;
+    }
+
+    public NodoGen getHermanoDerecho(){
+        return hermanoDerecho;
+    }
+
+    public void setElem(Object elemento){
+        elem = elemento;
+    }
+
+    public void setHijoIzquierdo(NodoGen izq){
+        hijoIzquierdo = izq;
+    }
+
+    public void setHermanoDerecho(NodoGen der){
+        hermanoDerecho = der;
+    }
+
+}
